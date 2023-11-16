64f9f30144acb3f0780440ecc490736cac4ad84f
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-11-16 21:51:46 +0500

    Update DiscordMobileLib.lua

--- DiscordMobileLib.lua
+++ DiscordMobileLib.lua
@@ -17,6 +17,14 @@ local function Toggle(Channel, Text, Default, Response)
 	Channel:Toggle(Text, Default, Response)
 end
 
+local function Slider(Channel, Text, Max, Min, Default, Response)
+    Channel:Slider(Text, Min, Max, Default, Response)
+end
+
+local function Dropdown(Channel, Text, Table, Response)
+	Channel:Slider(Text, Table, Response)
+end
+
 local function Notif(Title, Text, Response)
 	DiscordLib:Notification(Title, Text, Response)
 end
@@ -28,5 +36,24 @@ end)
 Tab(c1)
 
 Toggle(c1, "Toggle", false, function(v)
-	Notif("Toggle value change",  "Value is " .. (v), "Okay")
+	game.StarterGui:SetCore("SendNotification,{
+		Title = "Value Change",
+		Text = "Value changed to " .. v,
+		Duration = 5
+	})
 end)
+
+Tab(c1)
+
+Slider(c1, "Slider", 100, 16, 16, function(v)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (v)
+end)
+
+Tab(c1)
+
+local table1 = {thing1, thing2, thing3}
+
+Dropdown(c1, "Dropdown", table1, function(v)
+	Notif("Thingy", v, "okay")
+end)
+
