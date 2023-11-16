272fafc4af553cf9de14bbcdef3237016787f5cb
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-11-16 22:07:38 +0500

    Update DiscordMobileLib.lua

--- DiscordMobileLib.lua
+++ DiscordMobileLib.lua
@@ -1,10 +1,10 @@
-local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/NICKISBAD/ESPTagLib/main/ESPLib.lua"))(()
+local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/NICKISBAD/ESPTagLib/main/ESPLib.lua"))()
 
-local b = a:ESPLib:CreateESPTag({
+local b = ESPLib:CreateESPTag({
     		Text = "ExampleName",
     		Part = game.Workspace.Items.Arrow.Handle, -- Replace 'Part' with the actual reference to your part
     		TextSize = 7,
     		TextColor = Color3.new(255, 255, 255),
     		BoxColor = Color3.new(255, 255, 255),
-			TracerColor = Color3.new(255,255,255)
-		  })
+		TracerColor = Color3.new(255,255,255)
+	})
