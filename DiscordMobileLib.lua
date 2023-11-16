dc8791fa9c226317fe5f74601c186b53a481bbcc
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-11-16 21:55:51 +0500

    Update DiscordMobileLib.lua

--- DiscordMobileLib.lua
+++ DiscordMobileLib.lua
@@ -45,7 +45,7 @@ end)
 
 Tab(c1)
 
-Slider(c1, "Slider", 100, 16, 16, function(v)
+Slider(c1, "Slider", 16, 100, 16, function(v)
 	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (v)
 end)
 
